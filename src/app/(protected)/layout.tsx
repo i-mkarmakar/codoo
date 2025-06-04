69201596ea8dc1fc0d4118c239268fff31f00235
Manish Karmakar <mrinalkarmakar565@gmail.com>
2025-06-04 09:27:57 +0530

    feat: add sidebar layout with reusable components, user context, mobile support, and utility functions

---
+++ src/app/(protected)/layout.tsx
@@ -0,0 +1,26 @@
+import { SidebarProvider } from "@/components/ui/sidebar";
+import { UserButton } from "@clerk/nextjs";
+import React from "react";
+
+type Props = {
+  children: React.ReactNode;
+};
+
+const SidebarLayout = ({ children }: Props) => {
+  return (
+    <SidebarProvider>
+      <main className="m-2 w-full">
+        <div className="border-sidebar bg-sidebar flex items-center gap-2 rounded-md border p-2 px-4 shadow">
+          <div className="ml-auto"></div>
+          <UserButton />
+        </div>
+        <div className="h-4"></div>
+        <div className="border-sidebar-border bg-sidebar h-[calc(100vh-6rem)] overflow-y-scroll rounded-md border p-4 shadow">
+          {children}
+        </div>
+      </main>
+    </SidebarProvider>
+  );
+};
+
+export default SidebarLayout;
