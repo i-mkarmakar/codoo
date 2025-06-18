c4ce9b95366bed268ae794881dbe3109f7ce5944
Manish Karmakar <mrinalkarmakar565@gmail.com>
2025-06-18 15:43:13 +0530

    refactor(section): remove unused SectionSvg and crossesOffset prop

--- src/components/section.tsx
+++ src/components/section.tsx
@@ -1,12 +1,10 @@
 import type React from "react";
-import SectionSvg from "./section-svg";
 
 
 const Section = ({
 	className,
 	id,
 	crosses,
-	crossesOffset,
 	customPaddings,
 	children,
 }: {
@@ -29,12 +27,6 @@ const Section = ({
 
 			<div className="hidden absolute top-0 left-5 w-[0.0625rem] h-[calc(100%_+_30px)] dark:bg-[#26242C] bg-stone-200  pointer-events-none lg:block lg:left-16 xl:left-16" />
 			<div className="hidden absolute top-0 right-5 w-[0.0625rem] h-[calc(100%_+_30px)]  dark:bg-[#26242C] bg-stone-200  pointer-events-none lg:block lg:right-14 xl:right-14" />
-
-			{crosses && (
-				<>
-					<SectionSvg crossesOffset={crossesOffset} />
-				</>
-			)}
 		</div>
 	);
 };
